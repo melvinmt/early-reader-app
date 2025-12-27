15e0edf661b730d8023d9ce791cb8ca80c6edcf6
Melvin <mmtercan@gmail.com>
2025-12-27 02:20:33 -0500

    fix: Configure Metro to handle .webp files as non-image assets
    - Add webp to assetExts to prevent Metro from processing them as images
    - Fixes bundling error when Metro tries to process placeholder .webp text files

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Exclude .webp files from asset processing since they're placeholder text files
+config.resolver.assetExts = [...config.resolver.assetExts, 'webp'];
+
 module.exports = config;
